2006fd169b634767d6731c4b0e5f394a3c34a62e
sghung <sghung@ca.ibm.com>
2020-02-07 16:05:55 -0500

    [0.9.0] OpenAPI: Make use of Open Liberty Library (#131)

--- dev/org.eclipse.codewind.openapi.ui/src/org/eclipse/codewind/openapi/ui/commands/GenerateServerCommand.java
+++ dev/org.eclipse.codewind.openapi.ui/src/org/eclipse/codewind/openapi/ui/commands/GenerateServerCommand.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -18,6 +18,7 @@ import java.util.List;
 
 import org.eclipse.codewind.openapi.core.commands.CommandRunner;
 import org.eclipse.codewind.openapi.ui.Activator;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
@@ -44,6 +45,29 @@ public class GenerateServerCommand extends AbstractOpenApiGeneratorCommand {
 		mainArgsList.add("generate"); //$NON-NLS-1$
 		mainArgsList.add("-g"); //$NON-NLS-1$
 		mainArgsList.add(generatorType);
+		
+		boolean isLiberty = false;
+		boolean isAppsodyLiberty = false;
+        
+		IResource serverProjectFile = project.findMember("/src/main/liberty/config/server.xml"); //$NON-NLS-1$
+		if (serverProjectFile != null) {
+			isLiberty = serverProjectFile.exists();
+			
+			IResource appsodyProjectFile = project.findMember(".appsody-config.yaml"); //$NON-NLS-1$
+			
+			if (appsodyProjectFile != null) {
+				isAppsodyLiberty = isLiberty && appsodyProjectFile.exists();
+			}	
+		}
+		
+		if (generatorType.equals("jaxrs-spec") && (codewindProjectTypeId != null && codewindProjectTypeId.toLowerCase().indexOf("liberty") >= 0) //$NON-NLS-1$ //$NON-NLS-2$ 
+				|| (codewindProjectTypeId != null && codewindProjectTypeId.toLowerCase().indexOf("docker") >= 0 && isLiberty) //$NON-NLS-1$ 
+				|| (codewindProjectTypeId != null && codewindProjectTypeId.toLowerCase().indexOf("appsodyextension") >= 0 && isAppsodyLiberty)) { //$NON-NLS-1$
+			mainArgsList.add("--library"); //$NON-NLS-1$
+			mainArgsList.add("openliberty"); //$NON-NLS-1$
+			mainArgsList.add("--enable-post-process-file"); //$NON-NLS-1$
+		}
+		
 		mainArgsList.add("-i"); //$NON-NLS-1$
 		if (this.openApiFile != null) {
 			mainArgsList.add(argSurround + openApiFile.getLocation().toOSString() + argSurround);
