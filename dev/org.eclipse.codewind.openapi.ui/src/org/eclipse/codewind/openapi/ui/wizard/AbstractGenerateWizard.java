2b954a4da9867cfc63e42d585e6278601eca0d8f
Steven Hung <sghung@ca.ibm.com>
2020-02-07 16:02:49 -0500

    [0.9.0] OpenAPI: Make use of Open Liberty Library

--- dev/org.eclipse.codewind.openapi.ui/src/org/eclipse/codewind/openapi/ui/wizard/AbstractGenerateWizard.java
+++ dev/org.eclipse.codewind.openapi.ui/src/org/eclipse/codewind/openapi/ui/wizard/AbstractGenerateWizard.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 IBM Corporation and others.
+ * Copyright (c) 2019, 2020 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -90,6 +90,8 @@ public abstract class AbstractGenerateWizard extends Wizard implements INewWizar
 		}
 
 		PROJECT_TYPE projectType = page.getProjectType();
+		
+		cmd.setCodewindProjectTypeId(page.getCodewindProjectTypeId());
 
 		// Need a wrapper outside the context of the wizard page, since the wizard will be disposed of.
 		// Pass in the needed parameters gathered by the wizard
